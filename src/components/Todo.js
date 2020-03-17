a9c17a901635e113933d48767ca4232862976ec4
dqxy <yanofsky.david@gmail.com>
2020-03-16 21:05:55 -0400

    React

---
+++ src/components/Todo.js
@@ -0,0 +1,56 @@
+import React, { useState, useReducer } from "react";
+import { initialState, todoReducer } from "../reducers/todoReducer";
+
+const Todo = () => {
+    const [state, dispatch] = useReducer(todoReducer, initialState);
+    const [newTodo, updateNewTodo] = useState("");
+
+    const handleChanges = e => {
+        updateNewTodo(e.target.value);
+      };
+
+    return(
+        <div>
+            <div>
+                <input 
+                    type="text"
+                    name="newTodoText"
+                    value={newTodo}
+                    onChange={handleChanges}
+                />
+                <button
+                    onClick={() => dispatch({
+                        type: "ADD_ITEM",
+                        payload: newTodo
+                    })}>
+                    Add Item
+                </button>
+                <button
+                    onClick={() => dispatch({
+                        type: "CLEAR_DONE"
+                    })}>
+                    Clear Completed Tasks
+                </button>
+            </div>
+            <div className="todo-list">
+                <ul>
+                {state.map(item => (
+                    <li>
+                        <div 
+                            className={`todo-item ${item.completed? "done" : ""}`}
+                            onClick={() => dispatch({
+                                type: "TOGGLE_COMPLETED",
+                                payload: item.id
+                            })}>
+                            {item.item}
+                        </div>
+                    </li>
+                ))}
+                </ul>
+
+            </div>
+        </div>
+    )
+}
+
+export default Todo;
---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,27 @@
+export const initialState = [
+  {
+      item: "Make todo list",
+      completed: false,
+      id: 1
+  }
+];
+
+export const todoReducer = (state,action) => {
+  switch (action.type) {
+      case "TOGGLE_COMPLETED":
+          return [...state.map(item=>(
+              (item.id === action.payload)? 
+              {...item, completed: !item.completed} : 
+              {...item}))];
+      case "ADD_ITEM":
+          return [...state, {
+              item: action.payload,
+              completed: false,
+              id: new Date()
+          }];
+      case "CLEAR_DONE":
+          return state.filter(item=>!(item.completed));
+      default:
+          return state;
+  }
+}
